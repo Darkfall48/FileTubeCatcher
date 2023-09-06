9ed42eeb54abdddff68bc7c73e178eea545f9868
Darkfall48 <sidneysebban@gmail.com>
2023-09-06 23:08:45 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# PDF-To-YouTube-Video-Downloader
+ Automatically extract YouTube video links from PDF files and download the videos with a Python script. Simplify the process of accessing linked videos in PDFs, making it efficient for managing multiple documents with embedded YouTube content.
---
+++ Script/Pdf-To-Youtube.py
@@ -0,0 +1,50 @@
+import os
+import fitz  # PyMuPDF
+import re
+from pytube import YouTube
+
+
+def extract_youtube_links(pdf_path):
+    doc = fitz.open(pdf_path)
+    youtube_links = []
+
+    for page_num in range(doc.page_count):
+        page = doc[page_num]
+        text = page.get_text()
+
+        # Use a regular expression to find YouTube links
+        youtube_regex = r"(https?://(?:www\.)?youtube\.com/watch\?v=[\w-]+)"
+        youtube_links.extend(re.findall(youtube_regex, text))
+
+    return youtube_links
+
+
+def download_videos(youtube_links, output_folder):
+    for link in youtube_links:
+        try:
+            yt = YouTube(link)
+            stream = yt.streams.get_highest_resolution()
+            stream.download(output_folder)
+            print(f"Downloaded: {yt.title}")
+        except Exception as e:
+            print(f"Error downloading {link}: {e}")
+
+
+def process_pdfs_in_folder(folder_path, output_folder):
+    # Ensure the output folder exists
+    os.makedirs(output_folder, exist_ok=True)
+
+    # List all PDF files in the folder
+    pdf_files = [f for f in os.listdir(folder_path) if f.endswith(".pdf")]
+
+    for pdf_file in pdf_files:
+        pdf_path = os.path.join(folder_path, pdf_file)
+        youtube_links = extract_youtube_links(pdf_path)
+        download_videos(youtube_links, output_folder)
+
+
+if __name__ == "__main__":
+    folder_path = "Pdfs"  # Replace with the path to your folder of PDFs
+    output_folder = "Output"
+
+    process_pdfs_in_folder(folder_path, output_folder)
